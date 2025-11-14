be21f7b11c1d2d1c8016803f98ea220809de2b83
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-11-15 00:40:50 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+A = input()
+B = input()
+
+print(int(A)*int(B[2]))
+print(int(A)*int(B[1]))
+print(int(A)*int(B[0]))
+
+C = int(A)*int(B[2])
+D = int(A)*int(B[1])*10 
+E = int(A)*int(B[0])*100
+
+print(C + D + E) 
