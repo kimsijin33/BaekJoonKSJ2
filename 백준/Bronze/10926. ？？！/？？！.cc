1a2e3889c902ce926493868925ae19645beba6f8
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-12-07 18:50:01 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.cc
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int main() {
-    //printf("Hello world!\n");
-    char name[50];
-    char msg[] = "??!";
-    scanf("%s", name);
-    printf("%s%s\n", name, msg);
-    return 0;
+
+int main()
+{
+	char cha1[50];
+	char cha2[] = "??!";
+
+	scanf("%s", cha1);
+
+	printf("%s%s\n", cha1, cha2);
+
+	return 0;
 }
