b0e6a94f0975f9ea698ceb40a5daf6a57ecf8061
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-12-07 18:49:33 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    //printf("Hello world!\n");
+    char name[50];
+    char msg[] = "??!";
+    scanf("%s", name);
+    printf("%s%s\n", name, msg);
+    return 0;
+}
