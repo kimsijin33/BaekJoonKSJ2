50a97166477bef424f9815b129efcdc43f4ef2d9
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-12-10 23:00:53 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS    // scanf 보안 경고로 인한 컴파일 에러 방지
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    //printf("%d %d\n", num1, num2);
+    printf("%d\n", num1 - num2);
+
+    return 0;
+}
