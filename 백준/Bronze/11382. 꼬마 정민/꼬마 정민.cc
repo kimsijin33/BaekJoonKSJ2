6b87a274707739081d36974de00013d179a864d4
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-12-18 23:57:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    long num1, num2, num3;
+    scanf("%ld %ld %ld", &num1, &num2, &num3);
+    //printf("%d %d\n", num1, num2);
+    printf("%ld\n", num1 + num2 +num3);
+
+    return 0;
+}
