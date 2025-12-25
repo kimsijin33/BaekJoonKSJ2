2e7116a8394713f7696cc4ab6c7a87e9ac3048fb
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2025-12-25 22:44:23 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,23 @@
+//import java.util.*;
+//import java.lang.*;
+//import java.io.*;
+import java.util.Scanner;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        
+        int num1, num2;
+        Scanner sc = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        //int num1 = scanner.nextInt();
+        //int num2 = scanner.nextInt();
+        //scanf("%d %d", num1, num2);
+        //System.out.println("Hello world!");
+        System.out.println(num1 * num2);
+
+        sc.close();
+    }
+}
