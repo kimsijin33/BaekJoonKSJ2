7c6d5a74cfbdf68d0b7902176c90b6dc19b4924b
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2026-01-01 23:31:25 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+//class Main {
+//    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+//    }
+//}
+
+//import java.u//til.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        //long num1, num2, num3;
+        //Scanner sc = new Scanner(System.in);
+        //num1 = sc.nextLong();
+        //num2 = sc.nextLong();
+        //num3 = sc.nextLong();
+
+        //System.out.println(num1 + num2 + num3);
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        //sc.close();
+    }
+}
