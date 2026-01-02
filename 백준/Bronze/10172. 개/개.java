72bf6c7f7b554148e87c9f555760a40a3660ea15
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2026-01-02 23:24:36 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+//class Main {
+//    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+//    }
+//}
+
+//import java.u//til.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        //long num1, num2, num3;
+        //Scanner sc = new Scanner(System.in);
+        //num1 = sc.nextLong();
+        //num2 = sc.nextLong();
+        //num3 = sc.nextLong();
+
+        //System.out.println(num1 + num2 + num3);
+        
+        //System.out.println("\\    /\\");
+        //System.out.println(" )  ( ')");
+        //System.out.println("(  /  )");
+        //System.out.println(" \\(__)|");
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+        //sc.close();
+    }
+}
