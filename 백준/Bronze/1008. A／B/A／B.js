faa70df99d400427c545b04b6324600ff4d0edf7
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2026-01-07 22:59:35 +0900

    [Bronze V] Title: A/B, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,19 @@
+//const num1=prompt("첫 번째");
+//const num2=prompt("두 번재");
+//const add= Number(num1)+ Number(num2);
+//console.log(add);
+
+ // fs 모듈을 불러옵니다.
+const fs = require('fs');
+
+// /dev/stdin 파일을 읽어와 문자열로 변환한 뒤 공백이나 줄바꿈 기준으로 나눕니다.
+const input = fs.readFileSync('/dev/stdin').toString().split(/\s/);
+
+// 입력받은 값은 문자열이므로 숫자로 변환합니다.
+const num1 = Number(input[0]);
+const num2 = Number(input[1]);
+
+// 결과 출력
+console.log(num1 / num2);
+
+
