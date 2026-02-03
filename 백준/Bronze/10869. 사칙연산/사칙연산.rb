e88b6bac5cf317da316c72e978e9e45affb2f2bb
KimSiJin <46598332+kimsijin33@users.noreply.github.com>
2026-02-03 23:35:59 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 120372 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.rb
@@ -0,0 +1,18 @@
+num1, num2 = gets.split.map(&:to_i)
+plus = num1 + num2
+substraction = num1 - num2
+product = num1 * num2
+divide = num1 / num2
+remainder_operater = num1 % num2
+puts "#{plus}"
+puts "#{substraction}"
+puts "#{product}"
+puts "#{divide}"
+puts "#{remainder_operater}"
+
+#a, b = map(int, input().split( ))
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(int(a/b))
+#print(a%b)
